441ae61e26fd03b2ed9713bd5d7063e91c1bc844
Pavel <pavel.priladyshev@gmail.com>
2023-03-07 15:54:44 +0300

    feat: add path

---
+++ test-page/src/pages/OpenAccount/OpenAccount.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const OpenAccount = () => {
+  return (
+    <>
+      <div className="title">MAIN</div>
+    </>
+  );
+};
+
+export default OpenAccount;
