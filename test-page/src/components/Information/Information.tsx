66c45b7dc750c702bd323c84b7d51ff8213313f6
Pavel <pavel.priladyshev@gmail.com>
2023-03-08 12:09:59 +0300

    feat: add information aside block padding

--- test-page/src/components/Information/Information.tsx
+++ test-page/src/components/Information/Information.tsx
@@ -4,7 +4,26 @@ import './style.scss';
 const Information = () => {
   return (
     <>
-      <aside className="info-aside"></aside>
+      <aside className="info-aside">
+        <div className="info-aside-information">
+          <div className="info-aside-title">Информация</div>
+          <ul>
+            <li className="info-aside-content">Открытие счета без посещения офиса банка</li>
+            <li className="info-aside-content">Счет открывается в течение 15 минут</li>
+            <li className="info-aside-content">
+              После открытия по счету можно принимать и отправлять платежи
+            </li>
+          </ul>
+        </div>
+        <div className="info-aside-information">
+          <div className="info-aside-title">Преимущества</div>
+          <ul>
+            <li className="info-aside-content">До 3-х месяцев бесплатного обслуживания</li>
+            <li className="info-aside-content">До 500 000 ₽ на сервисы партнёров</li>
+            <li className="info-aside-content">Бесплатная онлайн-бухгалтерия</li>
+          </ul>
+        </div>
+      </aside>
     </>
   );
 };
