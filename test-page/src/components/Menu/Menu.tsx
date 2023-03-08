ab777d440f2a631faf044dc91e479d886df5a0f0
Pavel <pavel.priladyshev@gmail.com>
2023-03-08 12:40:26 +0300

    feat: add buttons to menu header

--- test-page/src/components/Menu/Menu.tsx
+++ test-page/src/components/Menu/Menu.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './style.scss';
+import plus from '../../assets/icons/plus.png';
+import download from '../../assets/icons/download.png';
 
 const Menu = () => {
   return (
     <>
-      <div className="menu"></div>
+      <div className="menu">
+        <div className="menu-header">
+          <button className="btn-menu-header">
+            <img className="plus" src={plus} alt="logo-plus" />
+            Создать
+          </button>
+          <button className="btn-menu-header">
+            <img className="download" src={download} alt="logo-download" />
+            Импорт
+          </button>
+        </div>
+      </div>
     </>
   );
 };
