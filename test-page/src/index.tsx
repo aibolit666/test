551dd599920a81e962e25a8e08878e3248bfecf3
Pavel <pavel.priladyshev@gmail.com>
2023-03-08 23:03:58 +0300

    refactor: path

--- test-page/src/index.tsx
+++ test-page/src/index.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/test">
       <App />
     </BrowserRouter>
   </React.StrictMode>
